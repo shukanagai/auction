d25114fa0c65923e2831d8a6239dd2d80b0c0a12
堀之内 貴史 <nagahisa.takafumi@gmail.com>
2022-11-11 23:16:42 +0900

    コンストラクタ作成

--- class/Auction.js
+++ class/Auction.js
@@ -1,24 +1,18 @@
 module.exports = class Auction {
-  constructor(){
-    // 車の名前
-    this._carName = null;
-    // 車のメーカー
-    this._catMaker = null;
-    // 車の車種
-    this._carModel = null;
-    // 車の乗車定員数
-    this._carCapacity = null;
-    // 車の走行年数
-    this._carDrivingYears = null;
-    // 車の走行距離
-    this._carMileage = null;
-    // 車のベースカラー
-    this._carBaseColor = null;
-    // 車の画像ファイルパス
-    this._carImgPath = null;
+  constructor() {
+    // オークションid
+    this._auctionId = null;
+    // 車両id
+    this._carId = null;
+    // 落札者id
+    this._purchaserId = null;
     // オークション開始日時
     this._auctionStartDateTime = null;
+    // オークション終了日時
+    this._auctionEndDateTime = null;
     // オークション開始価格
     this._auctionStartPrice = null;
+    // オークション終了価格
+    this._auctionEndPrice = null;
   }
 }
