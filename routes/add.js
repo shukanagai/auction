1d13983f316d683cf2cf0a7a2597642d0cc3bc83
永井秀佳 <nagaihideka@nagaihidekanoMacBook-Air.local>
2022-12-09 19:05:18 +0900

    masterの

--- routes/add.js
+++ routes/add.js
@@ -13,8 +13,9 @@ addRouter
   // 会員新規登録の処理
   .post('/clientInsert', async (req, res, next) => {
     const param = req.body;
+    console.log(param);
     await UserDao.insert(param.usName, param.usLoginId, param.usPassword, param.usGender, param.usBirthday, param.usAddress, param.usMail, param.usTel);
-    res.redirect('../../login');
+    res.redirect('./login');
   })
 ;
 
